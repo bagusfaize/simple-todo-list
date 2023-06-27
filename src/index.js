313d33eb4bd3616cf0b250a4cff9214aeb8b69e5
bagus faize <bagus.faize@gmail.com>
2023-06-27 11:34:13 +0700

    init project, add folder structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+import Home from './pages/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>
+  },
+  {
+    path: '/detail',
+    element: <div>detail</div>
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <div
+            style={{background:'#16ABF8', padding:'1rem 4rem', color:'white'}}
+        >
+            TO DO LIST APP
+        </div>
+        <div
+            style={{padding:'1rem 4rem'}}
+        >
+            Buat activity pertamamu
+        </div>
+    </div>
+  )
+}
