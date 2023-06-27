216e09b97b7603ce43bb9771eaa8b679a7b7fce1
bagus faize <bagus.faize@gmail.com>
2023-06-27 15:55:14 +0700

    add link to on card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import '../styles/card.scss';
 import {FiTrash2} from 'react-icons/fi';
+import {Link} from 'react-router-dom';
 
 export default function Card({title}) {
   return (
-    <div className="col-3">
+    <Link to={'/detail'} className="col-3">
       <div 
           className="activity-card"
           // data-cy="header-background"
-      >
+          >
         <div className="title">New Activity</div>
         <div className="footer">
           <span className="date">10 Juni 2023</span>
           <span className="delete"><FiTrash2 /></span>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
