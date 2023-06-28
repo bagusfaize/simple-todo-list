cf427f29310c13cc6676142cfb751dad6b7ede74
bagus faize <bagus.faize@gmail.com>
2023-06-28 13:21:21 +0700

    pass card data from props

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,17 +2,18 @@ import React from 'react';
 import '../styles/card.scss';
 import {FiTrash2} from 'react-icons/fi';
 import {Link} from 'react-router-dom';
+import moment from 'moment/moment';
 
-export default function Card({title}) {
+export default function Card({title, date}) {
   return (
     <Link to={'/detail'} className="col-3">
       <div 
           className="activity-card"
           // data-cy="header-background"
           >
-        <div className="title">New Activity</div>
+        <div className="title">{title}</div>
         <div className="footer">
-          <span className="date">10 Juni 2023</span>
+          <span className="date">{moment(date).format('DD MMMM YYYY')}</span>
           <span className="delete"><FiTrash2 /></span>
         </div>
       </div>
