d0dc6c249101a602c6f622209a5f317e997deadb
bagus faize <bagus.faize@gmail.com>
2023-06-28 14:20:46 +0700

    add header props on modal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,20 +3,22 @@ import { Modal } from 'react-bootstrap';
 import '../styles/modal.scss';
 
 export default function ModalComponent(props) {
-    const { show, onClose, children, modalTitle } = props;
-    console.log('clg props', props);
+    const { show, onClose, children, modalTitle, header } = props;
     return (
         <Modal
             show={show}
             onHide={onClose}
             onClose={onClose}
         >
-            <Modal.Header closeButton>
-                <Modal.Title>{modalTitle}</Modal.Title>
-                {/* <p className="btn-modal-close" onClick={onClose}>
-                    <i className="fa fa-times text-danger"></i>
-                </p> */}
-            </Modal.Header>
+            {
+                header &&
+                <Modal.Header closeButton>
+                    <Modal.Title>{modalTitle}</Modal.Title>
+                    {/* <p className="btn-modal-close" onClick={onClose}>
+                        <i className="fa fa-times text-danger"></i>
+                    </p> */}
+                </Modal.Header>
+            }
             <Modal.Body>{children}</Modal.Body>
         </Modal>
     )
