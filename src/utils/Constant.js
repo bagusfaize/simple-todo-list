6d51dde85524108e0c589b82f4f9bf3447bca9a4
bagus faize <bagus.faize@gmail.com>
2023-06-29 23:04:08 +0700

    add priority option constant

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,1 +1,29 @@
 export const BASE_URL = "https://todo.api.devcode.gethired.id";
+
+export const priorityOptions = [
+    {
+      label: "Very High",
+      value: "very-high",
+      icon: <div className='option-icon red'></div>
+    },
+    {
+      label: "High",
+      value: "high",
+      icon: <div className='option-icon yellow'></div>
+    },
+    {
+      label: "Normal",
+      value: "normal",
+      icon: <div className='option-icon green'></div>
+    },
+    {
+      label: "Low",
+      value: "low",
+      icon: <div className='option-icon blue'></div>
+    },
+    {
+      label: "Very Low",
+      value: "very-low",
+      icon: <div className='option-icon purple'></div>
+    },
+  ];
